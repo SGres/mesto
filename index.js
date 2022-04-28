823710addaa18ab5614ed4d60e8ee704bdd5ce12
gres-home-pc <gresalexrserg@gmail.com>
2022-04-28 07:37:11 +0300

    сделать нормальную ширину в названиипрофиле

--- index.js
+++ index.js
@@ -27,23 +27,5 @@ function formSubmitHandler(evt) {
 
 editButton.addEventListener('click', openForm);
 closeButton.addEventListener('click', closeForm);
-// nameSave.addEventListener('click', saveForm);
 submitForm.addEventListener('submit', formSubmitHandler);
 
-
-//     // Обработчик «отправки» формы, хотя пока
-//     // она никуда отправляться не будет
-//     function formSubmitHandler(evt) {
-//       evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-//       // Так мы можем определить свою логику отправки.
-//       // О том, как это делать, расскажем позже.
-
-//       // Получите значение полей jobInput и nameInput из свойства value
-
-//       // Выберите элементы, куда должны быть вставлены значения полей
-
-//       // Вставьте новые значения с помощью textContent
-//     }
-
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
