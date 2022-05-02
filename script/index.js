50750d3894f95ec7676f9af6ff92c6edcf73011a
gres-laptop-asus <gresalexrserg@gmail.com>
2022-05-02 16:45:56 +0300

    первая итерация 1.2

--- script/index.js
+++ script/index.js
@@ -4,8 +4,8 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__button-close');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let inputName = document.querySelector('.popup__input-name');
-let inputJob = document.querySelector('.popup__input-job');
+let inputName = document.querySelector('.popup__input_name');
+let inputJob = document.querySelector('.popup__input_job');
 let submitForm = document.querySelector('.popup__form');
 
 function openForm() {
